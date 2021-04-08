885cf1217659fa5703503d6ba6f88de23011f0d6
Arturo <arturodeza@gmail.com>
2021-04-08 17:20:43 -0400

    Create download_models_and_stimuli.sh

---
+++ download_models_and_stimuli.sh
@@ -0,0 +1,13 @@
+wget https://www.dropbox.com/s/ovuyfqumr5xymds/Full_Metamer_Stimuli.zip
+unzip Full_Metamer_Stimuli.zip
+https://www.dropbox.com/s/vktpuc16pj5stac/modelsPyTorch.zip
+unzip modelsPyTorch.zip
+mv modelsPyTorch models
+wget https://www.dropbox.com/s/rtovjv8y1xbyso6/Receptive_Fields.zip
+unzip Receptive_Fields.zip
+wget https://www.dropbox.com/s/5semltqozxywsvz/Refinement.zip
+unzip Refinement.zip
+rm Full_Metamer_Stimuli.zip
+rm modelsPyTorch.zip
+rm Receptive_Fields.zip
+rm Refinement.zip
